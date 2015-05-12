db790a0f6ade5851f636b7a01b35223829d735e4
Martin Morgan <mtmorgan@fhcrc.org>
2015-05-12 08:06:15 -0700

    dim-methods

--- R/binding.R
+++ R/binding.R
@@ -1,4 +1,8 @@
 setClass("A")
 setClass("B", contains="A")
+
 setMethod("show", "A", function(object) cat("A\n"))
 setMethod("show", "B", function(object) cat("B\n"))
+
+setMethod("dim", "A", function(x) "A-dim")
+setMethod("dim", "B", function(x) "B-dim")
