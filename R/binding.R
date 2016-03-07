ba0f5a72bbd78c0994a681b3fcc96eb36d024ea3
Martin Morgan <martin.morgan@roswellpark.org>
2016-03-07 09:24:08 -0500

    simple vignette

--- R/binding.R
+++
@@ -1,8 +0,0 @@
-setClass("A")
-setClass("B", contains="A")
-
-setMethod("show", "A", function(object) cat("A\n"))
-setMethod("show", "B", function(object) cat("B\n"))
-
-setMethod("dim", "A", function(x) "A-dim")
-setMethod("dim", "B", function(x) "B-dim")
